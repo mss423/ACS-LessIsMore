f2612af106a9600c2be7bda6d7d53c70a473e737
Max Springer <mss423@cornell.edu>
2024-12-31 11:04:49 -0500

    Bug fix in load_data

--- datasets/load_data.py
+++ datasets/load_data.py
@@ -4,7 +4,7 @@ import json
 
 def load_test_data(datadir, dataset="sst2"):
 	if dataset == "sst2":
-		path = os.pah.join(datadir,"SST2")
+		path = os.path.join(datadir,"SST2")
 		test_df = pd.read_csv(os.path.join(path,"test.tsv"),sep='\t',header=0)
 		test_df.columns = ['sentence','label']
 		return test_df
@@ -13,7 +13,7 @@ def load_test_data(datadir, dataset="sst2"):
 
 def load_train_data(datadir, dataset="sst2", synthetic=False):
 	if dataset == "sst2":
-		path = os.pah.join(datadir,"SST2")
+		path = os.path.join(datadir,"SST2")
 		train_df = pd.read_csv(os.path.join(path,"train.tsv"),sep='\t',header=0)
 		train_df.columns = ['sentence','label']
 		return train_df
