842eb0ad832c787fbbe5bc4bc90ad50581d3e358
Max Springer <mss423@cornell.edu>
2025-01-02 11:04:01 -0500

    Added fewrel data loading

--- datasets/load_data.py
+++ datasets/load_data.py
@@ -2,20 +2,126 @@ import os
 import pandas as pd
 import json
 
+FEWREL_LABELS = [
+    "P1001",
+    "P101",
+    "P102",
+    "P105",
+    "P106",
+    "P118",
+    "P123",
+    "P127",
+    "P1303",
+    "P131",
+    "P1344",
+    "P1346",
+    "P135",
+    "P136",
+    "P137",
+    "P140",
+    "P1408",
+    "P1411",
+    "P1435",
+    "P150",
+    "P156",
+    "P159",
+    "P17",
+    "P175",
+    "P176",
+    "P178",
+    "P1877",
+    "P1923",
+    "P22",
+    "P241",
+    "P264",
+    "P27",
+    "P276",
+    "P306",
+    "P31",
+    "P3373",
+    "P3450",
+    "P355",
+    "P39",
+    "P400",
+    "P403",
+    "P407",
+    "P449",
+    "P4552",
+    "P460",
+    "P466",
+    "P495",
+    "P527",
+    "P551",
+    "P57",
+    "P58",
+    "P6",
+    "P674",
+    "P706",
+    "P710",
+    "P740",
+    "P750",
+    "P800",
+    "P84",
+    "P86",
+    "P931",
+    "P937",
+    "P974",
+    "P991",
+]
+
+def load_train_data(datadir, dataset="sst2", synthetic=False):
+	fname = "syn-train" if synthetic else "train"
+
+	if dataset == "sst2":
+		path = os.path.join(datadir,"SST2")
+		train_df = pd.read_csv(os.path.join(path, fname+".tsv"),sep='\t',header=0)
+		train_df.columns = ['sentence', 'label']
+		return train_df
+	elif dataset == "fewrel":
+		path = os.path.join(datadir,"FewRel")
+		test_df = load_fewrel_data(os.path.join(data_path, fname+".json"))
+		return test_df
+	else:
+		raise ValueError('Invalid dataset name passed!')
+
 def load_test_data(datadir, dataset="sst2"):
 	if dataset == "sst2":
 		path = os.path.join(datadir,"SST2")
 		test_df = pd.read_csv(os.path.join(path,"test.tsv"),sep='\t',header=0)
 		test_df.columns = ['label','sentence']
 		return test_df
+	elif dataset == "fewrel":
+		path = os.path.join(datadir,"FewRel")
+		test_df = load_fewrel_data(os.path.join(data_path,"test.json"))
+		return test_df
 	else:
 		raise ValueError('Invalid dataset name passed!')
 
-def load_train_data(datadir, dataset="sst2", synthetic=False):
-	if dataset == "sst2":
-		path = os.path.join(datadir,"SST2")
-		train_df = pd.read_csv(os.path.join(path,"train.tsv"),sep='\t',header=0)
-		train_df.columns = ['sentence', 'label']
-		return train_df
-	else:
-		raise ValueError('Invalid dataset name passed!')
+# FewRel Data Functions #
+def linearize_input(text, head, tail):
+    return f"Head Entity : {head} , Tail Entity : {tail} , Context : {text}"
+
+
+def read_sample_dict(sample):
+    tokens = sample["tokens"]
+    head = " ".join([tokens[i] for i in sample["h"][2][0]])
+    tail = " ".join([tokens[i] for i in sample["t"][2][0]])
+    return " ".join(tokens), head, tail
+
+
+def load_fewrel_data(path):
+    pairs = []
+    with open(path) as f:
+        raw = json.load(f)
+        for label, lst in raw.items():
+            y = LABELS.index(label)
+            for sample in lst:
+                text, head, tail = read_sample_dict(sample)
+                x = linearize_input(text, head, tail)
+                pairs.append((x, y))
+
+    df = pd.DataFrame(pairs)
+    df.columns = ["sentence", "label"]
+    df = df.sample(frac=1)  # Shuffle
+    print(dict(path=path, data=df.shape, unique_labels=len(set(df["labels"].tolist()))))
+    return df
