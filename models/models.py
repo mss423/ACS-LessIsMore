47bb527ea43c7789e1868a551ab955efacea31e9
Max Springer <mss423@cornell.edu>
2025-01-07 10:16:03 -0500

    Update models.py
    Bug fix for handling multi-label classification

--- models/models.py
+++ models/models.py
@@ -37,23 +37,29 @@ class AlbertModel(nn.Module):
 class BertModel(nn.Module):
     def __init__(self, num_labels, requires_grad = True):
         super(BertModel, self).__init__()
-        if num_labels > 2:
-            self.bert = BertForSequenceClassification.from_pretrained('google-bert/bert-base-uncased',\
-                num_labels=num_labels, hidden_dropout_prob = 0.1, attention_probs_dropout_prob = 0.1,\
-                problem_type="multi_label_classification")
-        else:
-            self.bert = BertForSequenceClassification.from_pretrained('google-bert/bert-base-uncased',\
-                num_labels=num_labels, hidden_dropout_prob = 0.1, attention_probs_dropout_prob = 0.1)
+        self.bert = BertForSequenceClassification.from_pretrained(
+            'google-bert/bert-base-uncased',
+            num_labels=num_labels,
+            hidden_dropout_prob=0.1,
+            attention_probs_dropout_prob=0.1,
+            problem_type="multi_label_classification" if num_labels > 2 else None
+        )
         self.tokenizer = AutoTokenizer.from_pretrained('google-bert/bert-base-uncased', do_lower_case=True)
         self.requires_grad = requires_grad
-        self.device = torch.device("cuda")
+        self.device = torch.device("cuda" if torch.cude.is_available() else "cpu")
         for param in self.bert.parameters():
             param.requires_grad = requires_grad  # Each parameter requires gradient
 
     def forward(self, batch_seqs, batch_seq_masks, batch_seq_segments, labels):
-        loss, logits = self.bert(input_ids = batch_seqs, attention_mask = batch_seq_masks, 
-                              token_type_ids=batch_seq_segments, labels = labels)[:2]
-        probabilities = nn.functional.softmax(logits, dim=-1)
+        outputs = self.bert(
+            input_ids=batch_seqs.to(self.device), # move inputs to device
+            attention_mask=batch_seq_masks.to(self.device),
+            token_type_ids=batch_seq_segments.to(self.device),
+            labels=labels.to(self.device)
+        )
+        loss = outputs.loss
+        logits = outputs.logits
+        probabilities = torch.sigmoid(logits)
         return loss, logits, probabilities
         
         
