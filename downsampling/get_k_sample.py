0d23533e1be40bf8ce3bf5728275e93e5c9345f1
Max Springer <mss423@cornell.edu>
2025-04-23 11:42:08 -0400

    Fixed loading of synthetic data

--- downsampling/get_k_sample.py
+++ downsampling/get_k_sample.py
@@ -6,7 +6,7 @@ from kmeans_utils import *
 
 def get_k_sample(K, datapath, dataset="SST2", synthetic=False, method="random"):
 	data_path  = os.path.join(datapath, dataset)
-	syn_pre    = "syn" if synthetic else "human"
+	syn_pre    = "syn-" if synthetic else ""
 	sub_file   = method + "_" + syn_pre + f"_K{K}.tsv"
 
 	if method == "random":
@@ -18,7 +18,7 @@ def get_k_sample(K, datapath, dataset="SST2", synthetic=False, method="random"):
 		# Else run sampling on full datafile
 		except:
 			print("Running downsamping ...")
-			data_df = pd.read_csv(os.path.join(data_path, "train.tsv"), sep='\t')
+			data_df = pd.read_csv(os.path.join(data_path, syn_pre + "train.tsv"), sep='\t')
 			sub_df  = data_df.sample(n=K, random_state=42)
 			sub_df.to_csv(os.path.join(data_path,sub_file), sep='\t')
 
@@ -29,7 +29,7 @@ def get_k_sample(K, datapath, dataset="SST2", synthetic=False, method="random"):
 			print("Subsample exists, loading ...")
 		except:
 			print("Running downsamping ...")
-			data_df = pd.read_csv(os.path.join(data_path, "train.tsv"), sep='\t')
+			data_df = pd.read_csv(os.path.join(data_path, syn_pre + "train.tsv"), sep='\t')
 			sub_df  = kmeans_sample(data_df, K)
 			sub_df.to_csv(os.path.join(data_path,sub_file), sep='\t')
 
@@ -39,7 +39,7 @@ def get_k_sample(K, datapath, dataset="SST2", synthetic=False, method="random"):
 			print("Subsample exists, loading ...")
 		except:
 			print("Running downsamping ...")
-			data_df = pd.read_csv(os.path.join(data_path, "train.tsv"), sep='\t')
+			data_df = pd.read_csv(os.path.join(data_path, syn_pre + "train.tsv"), sep='\t')
 			sub_df  = entropy_sample(data_df, K)
 			sub_df.to_csv(os.path.join(data_path,sub_file), sep='\t')
 
@@ -49,7 +49,7 @@ def get_k_sample(K, datapath, dataset="SST2", synthetic=False, method="random"):
 			print("Subsample exists, loading ...")
 		except:
 			print("Running downsamping ...")
-			data_df = pd.read_csv(os.path.join(data_path, "train.tsv"), sep='\t')
+			data_df = pd.read_csv(os.path.join(data_path, syn_pre + "train.tsv"), sep='\t')
 			sub_df  = acs_sample(data_df, K)
 			sub_df.to_csv(os.path.join(data_path,sub_file), sep='\t')
 
